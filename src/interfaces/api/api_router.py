2d56d7ed5043e1e2f4642ed0522fd512d55b29e6
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2025-04-25 10:39:13 -0300

    feat: configuração para CI/CD azure

--- src/interfaces/api/api_router.py
+++ src/interfaces/api/api_router.py
@@ -2,16 +2,18 @@ from fastapi import APIRouter
 from fastapi import Depends
 from services.auth_service import get_current_user
 from models.account_model import Account
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+teste = os.getenv("TESTE")
 
 
 api_router = APIRouter()
 
 @api_router.get("/")
-def read_root(current_user: Account = Depends(get_current_user)):
+def read_root():
     return {
-        "health": "OK",
-        "user": {
-            "id": current_user.id,
-            "email": current_user.email
-        }
+        "Message": teste        
     }
